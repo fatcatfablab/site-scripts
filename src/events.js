79c407f76d000416ef686970076ab6a5471aa8ea
David Choi <dwchoi@gmail.com>
2023-02-10 01:17:00 -0500

    Update events.js

--- src/events.js
+++ src/events.js
@@ -57,7 +57,7 @@
                     (event) => `
                     <option value="${event.registration_url}">${date(
                       event.start_date
-                    )} - ${date(event.end_date)} (${event.seats_remaining} of ${
+                    ).split(", ")[0]} at ${date(event.start_date).split(", ")[1]} - ${date(event.end_date).split(", ")[1]} (${event.seats_remaining} of ${
                       event.max_participants
                     } seats remaining)</option>`
                   )}
