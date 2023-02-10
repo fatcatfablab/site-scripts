355eae2453b6fa492660f6ed408d3e7c9921432a
David Choi <dwchoi@gmail.com>
2023-02-10 01:37:28 -0500

    Update events.js

--- src/events.js
+++ src/events.js
@@ -3,6 +3,7 @@
     return new Intl.DateTimeFormat("en-US", {
       dateStyle: "full",
       timeStyle: "short",
+      hour: "numeric",
     }).format(new Date(d.replace(/-/g, "/")));
   };
 
@@ -57,7 +58,7 @@
                     (event) => `
                     <option value="${event.registration_url}">${date(
                       event.start_date
-                    )} - ${date(event.end_date).split(" ")[3]} (${event.seats_remaining} of ${
+                    )} - ${date(event.end_date)} (${event.seats_remaining} of ${
                       event.max_participants
                     } seats remaining)</option>`
                   )}
